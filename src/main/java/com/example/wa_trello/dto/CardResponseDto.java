ace53f417a10819635d5224aca4d2b7c1bde9cd1
WeGeon <wigon99@gmail.com>
2021-08-30 22:40:30 +0900

    First Dto Develop
    처음 Dto 작성

--- src/main/java/com/example/wa_trello/dto/CardResponseDto.java
+++ src/main/java/com/example/wa_trello/dto/CardResponseDto.java
@@ -1,4 +1,23 @@
 package com.example.wa_trello.dto;
 
+import com.example.wa_trello.entity.Card;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.awt.*;
+
+@Getter
+@NoArgsConstructor
 public class CardResponseDto {
+    private String title;
+    private int order;
+    private Long id;
+
+    @Builder
+    public CardResponseDto(String title, int order, long id) {
+        this.title = title;
+        this.order = order;
+        this.id = id;
+    }
 }
