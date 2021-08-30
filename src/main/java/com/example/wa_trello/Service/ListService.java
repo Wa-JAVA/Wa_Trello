e686853554458e20e1cfb1e852134170fbe9c234
HanSungHyun <han990102@naver.com>
2021-08-30 09:30:43 +0900

    feat: Create ToDoListService and CardService
    ToDoListService에서는
    
    Long toDoListSave (ToDoListSaveRequestDto)
    List<ToDoListResponseDto> toDoListFindAll ()
    ToDoListResponseDto toDoListFindById (Long)
    void toDoListDelete (ToDoListDeleteDto)
    Long toDoListUpdate (Long , ToDoListUpdateDto)
    
    총 5가지 메소드 지원
    
    CardService에서는
    
    Long CardSave (CardSaveRequestDto)
    CardResponseDto cardFindById (Long)
    void cardDelete (CardDeleteDto)
    Long cardUpdate (Long , CardUpdateRequestDto)
    
    총 4가지 메소드 지원

--- src/main/java/com/example/wa_trello/Service/ListService.java
+++ src/main/java/com/example/wa_trello/Service/ListService.java
@@ -1,4 +1,48 @@
 package com.example.wa_trello.Service;
 
+import com.example.wa_trello.dto.ToDoListDeleteDto;
+import com.example.wa_trello.dto.ToDoListResponseDto;
+import com.example.wa_trello.dto.ToDoListSaveRequestDto;
+import com.example.wa_trello.dto.ToDoListUpdateRequestDto;
+import com.example.wa_trello.entity.ToDoList;
+import com.example.wa_trello.repository.ToDoListRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ListService {
+    private ToDoListRepository toDoListRepository;
+
+    @Transactional
+    public Long toDoListSave(ToDoListSaveRequestDto toDoListSaveRequestDto){
+        return toDoListRepository.save(toDoListSaveRequestDto.toEntity()).getId;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ToDoListResponseDto> toDoListFindAll(){
+        return toDoListRepository.findAll()
+                .Stream()
+                .map(ToDoListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public ToDoListResponseDto toDoListFindById(Long id){
+        ToDoList toDoList = toDoListRepository.findById(id);
+        return ToDoListResponseDto(toDoList);
+    }
+
+    @Transactional
+    public void toDoListDelete(ToDoListDeleteDto toDoListDeleteDto) {
+        Long id = ToDoListDeleteDto.getId;
+        toDoListRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Long toDoListUpdate(Long id, ToDoListUpdateRequestDto toDoListUpdateRequestDto){
+        toDoListRepository.update(id,toDoListUpdateRequestDto.getTitle);
+        return id;
+    }
+
 }
